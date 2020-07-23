e1de087aa5cc31aba556b0704e0eaeae370c35a0
Rachna <rachgupta200@gmail.com>
2020-07-23 15:00:18 -0500

    Create descriptors.py

---
+++ descriptors.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pickle
+#load in pickle file with resnet18
+with open("resnet18_features.pkl", mode="rb") as opened_file:
+    resnet = pickle.load(opened_file)
+
+def generate_descriptor(imgID):
+    """ Generates decriptor from resnet dictionary if imgID is a key. Else, returns none 
+        
+    Parameters
+    ----------
+    imgID: int
+        the image id
+            
+    Returns
+    -------
+    descriptor: np.ndarray shape-(1,512)
+        the descriptor associated with the image id
+    or
+    
+    None
+        
+    """
+    if imgID in resnet.keys():
+        return resnet[imgID]
+    else:
+        return None
