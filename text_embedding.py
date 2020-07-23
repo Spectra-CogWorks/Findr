feecf36700693eb87d8aad6289030587085f5207
Esther <esthershen03@gmail.com>
2020-07-23 16:00:16 -0400

    Add docstrings

--- text_embedding.py
+++ text_embedding.py
@@ -8,22 +8,37 @@ glove = KeyedVectors.load_word2vec_format(path, binary=False)
 
 punc_regex = re.compile('[{}]'.format(re.escape(string.punctuation)))
 
-def strip_punc(corpus):
-    """ Removes all punctuation from a string.
+def strip_punc(text):
+    """ 
+    Removes all punctuation from a string.
 
-        Parameters
-        ----------
-        corpus : str
+    Parameters
+    ----------
+    text : str
+        The text to be stripped of punctuation.
 
-        Returns
-        -------
-        str
-            the corpus with all punctuation removed"""
-    # substitute all punctuation marks with ""
-    return punc_regex.sub('', corpus)
+    Returns
+    -------
+    str
+        the corpus with all punctuation removed
+    """
+    # substitutes all punctuation marks with ""
+    return punc_regex.sub('', text)
 
 def create_text_embedding(text):
-    
+    """
+    Creates text embeddings of captions and query text.
+
+    Parameters
+    ----------
+    text : str
+        The text to be converted into word embeddings.
+
+    Returns
+    -------
+    embeddings : np.ndarray
+        A shape-(1, 50) numpy array of embeddings for the input text, weighed according to each word's IDF.
+    """
     text = text.lower()
     text = strip_punc(text)
     text_array = text.split()
